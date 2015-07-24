66239ec5ac18c1a0f76b52fe53dac7c43faa4bcc
Arthur Schmitt <tart2000design@gmail.com>
2015-07-24 14:40:49 -0400

    mail link added

--- site/templates/home.php
+++ site/templates/home.php
@@ -18,6 +18,7 @@
 	</div>
 	<div id="socialz" class="card">
 		<div class="social-cont">
+			<a href="mailto:hi@arthurschmitt.com"><i class="fa fa-3x fa-envelope"></i></a>
 			<?php foreach (page('socials')->children() as $so) : ?>
 				<a href="<?php echo $so->solink() ?>" target="_blank"><i class="fa fa-<?php echo $so->fontawesome() ?> fa-3x"></i></a>
 			<?php endforeach ?>
